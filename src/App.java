b6d4e48a09d88df2de8718fc0a9a4af6e0c9a790
Artur Magioli <arturdepaula00@gmail.com>
2025-04-05 13:06:16 -0300

    anatomia-classes-3

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+    public static void main (String [] args) {
+
+        String primeiroNome = "Artur";
+        String segundoNome = "Magioli";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.print(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
