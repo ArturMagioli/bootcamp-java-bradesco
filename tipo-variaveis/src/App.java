16edd7c908ee41090db0859b4c9cb63d7d0486cd
Artur Magioli <arturdepaula00@gmail.com>
2025-04-05 16:16:04 -0300

    tipos-variaveis-4

--- tipo-variaveis/src/App.java
+++ tipo-variaveis/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Escrita de números padrão:
+        double salarioMinimo = 2500;
+
+        //Linguagem fortemente tipada: 
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto; //funciona
+        numeroCurto = (short) numeroNormal; //Erro. Necessita de uma casting (short)
     }
 }
