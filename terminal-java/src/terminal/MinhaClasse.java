9dda8e0582a1520cfa9513ad77c97bb13de707b8
Artur Magioli <arturdepaula00@gmail.com>
2025-04-09 21:03:46 -0300

    terminal java

---
+++ terminal-java/src/terminal/MinhaClasse.java
@@ -0,0 +1,10 @@
+package terminal;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Oi, fui executado pelo terminal");
+    }
+
+    //Fato interessante: Como 'MinhaClasse' está dentro do package 'terminal', executamos
+    //o 'MinhaClasse.class', dentro do bin, como 'java terminal.MinhaClasse'!
+}
