7993ef4ff32bc759f858587baecfff6c58a23af4
Artur Magioli <arturdepaula00@gmail.com>
2025-04-13 12:41:07 -0300

    feat: entrandoEmContato

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,12 +1,37 @@
 package candidatura;
 
 import java.util.concurrent.ThreadLocalRandom;
-
-import javax.swing.plaf.synth.SynthToolBarUI;
+import java.util.Random;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        imprimirSelecionados();
+        String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+        for(String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
+    }
+
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando)
+                tentativasRealizadas++;
+            else    
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+        }while(continuarTentando && tentativasRealizadas < 3);
+
+        if(atendeu)
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVA");
+        else
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + ", NÚMERO MAXIMO TENTATIvA " + tentativasRealizadas + " REALIZADA");
+    }
+
+    static boolean atender() {
+        return new Random().nextInt(3)==1;
     }
 
     static void imprimirSelecionados() {
