31e64f949f8da02246b4ef0fdbbb2d03714cc1c2
Artur Magioli <arturdepaula00@gmail.com>
2025-04-15 21:49:43 -0300

    heranca

--- pilares-poo/src/encapsulamento/ComputadorPedrinho.java
+++ pilares-poo/src/encapsulamento/ComputadorPedrinho.java
@@ -1,14 +1,21 @@
 package encapsulamento;
 
-import encapsulamento.MSNMessenger;
+import heranca.FacebookMessenger;
+import heranca.Telegram;
 
 public class ComputadorPedrinho {
     public static void main(String[] args) {
         //abrindo MSN Messeger
         MSNMessenger msn = new MSNMessenger();
-
         msn.enviarMensagem();
-
         msn.receberMensagem();
+
+        FacebookMessenger fcb = new FacebookMessenger();
+        fcb.enviarMensagem();
+        fcb.receberMensagem();
+
+        Telegram tlg = new Telegram();
+        tlg.enviarMensagem();
+        tlg.receberMensagem();
     }
 }
--- pilares-poo/src/encapsulamento/MSNMessenger.java
+++ pilares-poo/src/encapsulamento/MSNMessenger.java
@@ -1,20 +1,6 @@
 package encapsulamento;
 
-public class MSNMessenger {
-    public void enviarMensagem() {
-        validarConectadoInternet();
-        System.out.println("Enviando mensagem");
-        salvarHistoricoMensagem();
-    }
-    public void receberMensagem() {
-        System.out.println("Recebendo mensagem");
-    }
-    private void validarConectadoInternet() {
-        System.out.println("Validando se está conectado a internet");
-    }
-    private void salvarHistoricoMensagem() {
-        System.out.println("Salvanco o histórico da mensagem");
-    }
+import heranca.ServicoMensagemInstantanea;
 
-
+public class MSNMessenger extends ServicoMensagemInstantanea{
 }
